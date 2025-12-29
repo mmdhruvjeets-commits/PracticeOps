c64a09e1e6a464ebd17241f137f8ddc1fdf70a12
Krusha Shah <krusha.s@gormalone.com>
2025-12-29 18:56:44 +0530

    Changes all the files

--- obj/Debug/net8.0/PracticeOps.AssemblyInfo.cs
+++ obj/Debug/net8.0/PracticeOps.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticeOps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bbefc308535a2422c7c726d004aff2204b3245b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticeOps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticeOps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
